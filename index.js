8dffe95a1d0cb1dd9bb2071123426d39fff4bc9a
mgDentist <gmi1987@gmail.com>
2023-07-03 15:46:42 +0400

    done 1st home task

---
+++ index.js
@@ -0,0 +1,8 @@
+console.log('timer is running');
+alert(5);
+alert(4);
+alert(3);
+alert(2);
+alert(1);
+alert(0);
+console.log('timer is finished');
---
+++ js-info.js
@@ -0,0 +1,5 @@
+alert('Brendan Eich is an American computer programmer and technology executive. He created the JavaScript programming language and co-founded the Mozilla project, the Mozilla Foundation, and the Mozilla Corporation.');
+alert('Brandan Eich developed a new scripting language in just 10 days');
+alert('In September 1995, a Netscape programmer named Brandan Eich developed a new scripting language');
+alert('JavaScript is not Java');
+alert('The standards for JavaScript are the ECMAScript Language Specification (ECMA-262) and the ECMAScript Internationalization API specification (ECMA-402).');
